d35c0fbb19c6f206a44e15632a86e19a1f360750
John McLaughlin <john.mclaughlin@pqa.ca>
2018-06-29 11:49:20 -0300

    updates for docker

--- conf.js
+++ conf.js
@@ -1,31 +1,27 @@
 exports.config = {
     framework: 'mocha',
-    //chromeDriver: './node_module/.bin/chromedriver',
-    // capabilities: {
-    //     browserName: 'chrome',
-    //     chromeOptions: {
-    //         args: ["--start-maximized", "--disable-gpu"]
-    //     },
-    //     shardTestFiles: true,
-    //     maxInstances: 4
-    // },
-    'autoStartStopServer': true,
-    
+    chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.40',
     capabilities: {
-        'browserName': 'internet explorer',
-        'platform': 'ANY',
-        'version': '11',
-        'ignoreProtectedModeSettings': true
+        browserName: 'chrome',
+        chromeOptions: {
+            args: ["--headless", "--no-sandbox", "--disable-dev-shm-usage"]
+        },
+        shardTestFiles: true,
+        maxInstances: 4
     },
+    'autoStartStopServer': true,
+
+    // capabilities: {
+    //     'browserName': 'internet explorer',
+    //     'platform': 'ANY',
+    //     'version': '11',
+    //     'ignoreProtectedModeSettings': true
+    // },
     restartBrowserBetweenTests: false,
-    specs: ['paper-deep-spec.js'],
-    seleniumAddress: 'http://localhost:4444/wd/hub/',
+    specs: ['spec.js'],
     mochaOpts: {
         reporter: 'spec',
         timeout: 30000
     },
-    seleniumSeverJar: './node_modules/selenium-server/lib/runner/selenium-server-standalone-3.6.0.jar',
-    onPrepare: function() {
-        require('./lib/waitReady')
-    }
+    directConnect: true
 };
