d6bb74620fb5b41abec046174e0b5914ac5e6474
RyanJoe <39413888+RyanJoe@users.noreply.github.com>
2018-07-16 09:28:55 -0300

    t

--- conf.js
+++ conf.js
@@ -1,18 +1,18 @@
 exports.config = {
     framework: 'mocha',
     'autoStartStopServer': true,
-    directConnect: true,
+    //directConnect: true,
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-            args: ["--headless", "--start-maximized", "--disable-gpu", "--no-sandbox"]
+            args: ["--start-maximized", "--disable-gpu", "--no-sandbox"]
         },
         shardTestFiles: true,
         maxInstances: 4
     },
     restartBrowserBetweenTests: false,
     specs: ['paper-light-spec.js'],
-    // seleniumAddress: 'http://localhost:4444/wd/hub/',
+    seleniumAddress: 'http://localhost:4444/wd/hub/',
     mochaOpts: {
         reporter: 'spec',
         timeout: 30000
