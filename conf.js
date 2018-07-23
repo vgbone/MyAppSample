32b720d2b3c4980509208c2c4dd4173804dad211
John McLaughlin <john.mclaughlin@pqa.ca>
2018-07-23 08:04:31 -0300

    latest

--- conf.js
+++ conf.js
@@ -4,23 +4,22 @@ exports.config = {
     framework: 'jasmine',
     allScriptsTimeout: 99999,
     getPageTimeout: 30000,
-    multiCapabilities: [
-        // {
-        //     'browserName': 'firefox',
-        //     count: 1,
-        //     shardTestFiles: false,
-        //     maxInstances: 2
-        // },
-        {
-            'browserName': 'chrome',
+    multiCapabilities: [{
+            'browserName': 'firefox',
             count: 1,
-            shardTestFiles: true,
+            shardTestFiles: false,
             maxInstances: 2
-        }
+        },
+        // {
+        //     'browserName': 'chrome',
+        //     count: 1,
+        //     shardTestFiles: true,
+        //     maxInstances: 2
+        // }
     ],
     specs: ['Athennian-light-spec.js'],
-    // seleniumAddress: 'http://pqa-0352lt:4444/wd/hub/',
-    seleniumAddress: 'http://localhost:4444/wd/hub/',
+    seleniumAddress: 'http://pqa-0352lt:4444/wd/hub/',
+    // seleniumAddress: 'http://localhost:4444/wd/hub/',
     // Options to be passed to Jasmine.
     jasmineNodeOpts: {
         showColors: true,
